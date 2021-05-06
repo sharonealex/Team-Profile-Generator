23a95f3125c80743a7c3eb3811cfe1792fdd201c
Sharon <sharonelizalex@gmail.com>
2021-05-07 09:48:06 +1000

    initialised node app

---
+++ server.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+
+//function to get all team memeber details using inquirer package.
+
+
+//import the html template for each role
+
+//populate the html template with values from the previous
+
+//initialise app.
