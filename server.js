a3de6e3a98583638be334cfb6e7cd66a406cec0a
Sharon <sharonelizalex@gmail.com>
2021-05-07 12:18:02 +1000

    added list of general questions

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 const inquirer = require('inquirer');
 
+const generalQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please provide team member's name",
+    },
+    {
+        type: "list",
+        name: "role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        message: "Please select team memeber's role"
+    },
+    {
+        type: "input",
+        name: "emailID",
+        message: "Please provide team members's email address"
+    },
+    {
+        type: "input",
+        name: "TeamMemberId",
+        message: "Please provide team memeber ID"
+    }
+]
+
 //function to get all team memeber details using inquirer package.
 
 
@@ -8,3 +36,10 @@ const inquirer = require('inquirer');
 //populate the html template with values from the previous
 
 //initialise app.
+
+function init(){
+
+}
+
+
+init();
