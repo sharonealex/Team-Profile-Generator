5f931fd1c9e128e8834f884257be567b0902195f
Sharon <sharonelizalex@gmail.com>
2021-05-09 18:41:06 +1000

    first attempt to display html card

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fsPromises = require('fs').promises;
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -90,15 +91,54 @@ function getInternInputs(internQuestions) {
 
 //import the html template for each role
 
-function getBaseHtml(){
+function addRoleBasedHtmlCard(memberData) {
+    return new Promise((resolve, reject)=>{
+        const name = memberData.getName();
+        const role = memberData.getRole();
+        const emailId = memberData.getEmailId();
+        const employeeId = memberData.getEmployeeId();
 
+        let data = '';
+        if(role === 'Manager'){
+            const officeNumber = memberData.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />${role}</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${employeeId}</li>
+                <li class="list-group-item">Email Address: ${emailId}</li>
+                <li class="list-group-item">Office Phone: ${officeNumber}</li>
+            </ul>
+            </div>
+        </div>`
+        console.log("added manager card");
+        fsPromises.appendFile("./output/team.html", data)
+        .then((data)=>{
+            console.log("html appended..")
+        })
+        .catch((err)=>{
+            console.log("error" + err);
+        })
+        }
+      
+    });
 }
+
+function writeBaseHtmlToFile(data) {
+    const fileName = "./output/team.html";
+    return fsPromises.writeFile(fileName, data)
+}
+
+
 //populate the html template with values from the previous
 
 //initialise app.
 
 function init(){
-    getBaseHtml();
+    writeBaseHtmlToFile(baseHtml)
+    .catch((err)=>{
+        console.log(err)
+    })
     getTeamMemberInputs(generalQuestions)
     .then(({ name, role, emailID, employeeId })=>{
         let teamMember;
@@ -127,7 +167,10 @@ function init(){
     })
     .then(({teamMember, moreMembers})=>{
         teamMembers.push(teamMember);
-    
+        addRoleBasedHtmlCard(teamMember)
+        .catch((err)=>{
+            console.log(err);
+        })
         if(moreMembers === "yes"){
             getTeamMemberInputs(generalQuestions);
         }
